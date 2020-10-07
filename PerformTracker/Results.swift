20d690bafab185fd3a74467da359dd7e9dde1f68
Nihal Erdal <nihalerdall@gmail.com>
2020-10-06 20:31:26 -0700

    created UI Storyboard

---
+++ PerformTracker/Results.swift
@@ -0,0 +1,14 @@
+//
+//  Results.swift
+//  PerformTracker
+//
+//  Created by Nihal Erdal on 10/6/20.
+//  Copyright © 2020 Nihal Erdal. All rights reserved.
+//
+
+import Foundation
+
+struct Result {
+    var countOfWin: Int
+    var countOfLose:Int
+}
